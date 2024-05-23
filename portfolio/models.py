5a619ecc526771bc44e422eeecfd6c53a944852f
Dezlink <s.v.korotenko@yandex.ru>
2024-05-24 00:05:20 +0900

    portf + blog

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    descriotion = models.CharField(max_length=250)
-    image = models.ImageField9upload_to=('portfolio/images/')
+    description = models.CharField(max_length=250)
+    image = models.ImageField(default='portfolio/images/', upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
